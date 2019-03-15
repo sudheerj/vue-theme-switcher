3fb838c9c200c51897951dcc3530798551f44785
sudheerj <jsudheer50@gmail.com>
2019-03-16 00:43:10 +0800

    Add loaders in webpack

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import store from './store';
 import Element from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en'
 import './components/styled-components';
+import './element-variables.scss';
 
 Vue.config.productionTip = false;
 Vue.use(Element, {locale});
